b62a52de724b0341de9aeb65decb2658dffd9059
Konrad Kolasa <kolasakonrad1@gmail.com>
2023-11-09 23:01:30 +0100

    feat(fonts): setup main project font and font sizes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    css: [
-        '@/assets/styles/reset.scss'
-    ]
+    css: ['@/assets/styles/global.scss', '@/assets/styles/reset.scss', '@/assets/fonts/fonts.css']
 });
